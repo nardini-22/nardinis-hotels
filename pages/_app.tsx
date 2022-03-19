4271f34693b9945def600ac94fb147b5c5af40fa
ArSaN <arthnardini24@gmail.com>
2022-03-19 10:02:31 -0300

    Feature/Added-global-styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "../styles/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Head>
+        <title>Nardinis Hotels</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
