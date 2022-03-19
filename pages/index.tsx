438d7306a47550fefd92532eb39018d1c522a895
ArSaN <arthnardini24@gmail.com>
2022-03-19 15:02:02 -0300

    Feature/Added-new-redux-actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,7 @@
-import { GetHotelsList } from "actions/hotelsActions";
-import _ from "lodash";
 import type { NextPage } from "next";
-import { useEffect } from "react";
-import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 
 const Home: NextPage = () => {
-  const dispatch = useDispatch();
-  const hotelsList = useSelector((state: RootStateOrAny) => state.HotelsList);
-  useEffect(() => {
-    getData();
-  }, []);
-  const getData = (id = 1032) => {
-    dispatch(GetHotelsList(id));
-  };
-  const showData = () => {
-    if (hotelsList.loading) {
-      return <p>loading</p>;
-    }
-    if (!_.isEmpty(hotelsList.data)) {
-      return (
-        <>
-          <button onClick={() => console.log(hotelsList.data)}></button>
-        </>
-      );
-    }
-
-    if (hotelsList.errorMsg !== "") {
-      return <p>{hotelsList.errorMsg}</p>;
-    }
-    return null;
-  };
-  return <>{showData()}</>;
+  return <></>;
 };
 
 export default Home;
---
+++ src/reducer/hotelsDetailsReducer.tsx
@@ -0,0 +1,34 @@
+const DefaultState = {
+    loading: false,
+    data: [],
+    errrorMsg: "",
+  };
+  
+  const HotelsDetailsReducer = (state = DefaultState, action: any) => {
+    switch (action.type) {
+      case "HOTELS_DETAILS_LOADING":
+        return {
+          ...state,
+          loading: true,
+          errorMsg: "",
+        };
+      case "HOTELS_DETAILS_FAIL":
+        return {
+          ...state,
+          loading: false,
+          errorMsg: "Error!",
+        };
+      case "HOTELS_DETAILS_SUCCESS":
+        return {
+          ...state,
+          loading: false,
+          data: action.payload.result,
+          errorMsg: "",
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default HotelsDetailsReducer;
+  
