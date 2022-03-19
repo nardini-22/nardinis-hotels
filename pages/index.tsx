148a64c377480895eae28c8a14f6396fbddcd630
ArSaN <arthnardini24@gmail.com>
2022-03-19 12:25:05 -0300

    Bugfix/Fixed-redux-actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
+import { GetHotelsList } from "actions/hotelsActions";
+import _ from "lodash";
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 
 const Home: NextPage = () => {
-  return <></>;
+  const dispatch = useDispatch();
+  const hotelsList = useSelector((state: RootStateOrAny) => state.HotelsList);
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = (id = 1032) => {
+    dispatch(GetHotelsList(id));
+  };
+  const showData = () => {
+    if (hotelsList.loading) {
+      return <p>loading</p>;
+    }
+    if (!_.isEmpty(hotelsList.data)) {
+      return (
+        <>
+          <button onClick={() => console.log(hotelsList.data)}></button>
+        </>
+      );
+    }
+
+    if (hotelsList.errorMsg !== "") {
+      return <p>{hotelsList.errorMsg}</p>;
+    }
+    return null;
+  };
+  return <>{showData()}</>;
 };
 
 export default Home;
--- src/actions/hotelsActions.tsx
+++ src/actions/hotelsActions.tsx
@@ -1,10 +1,10 @@
-// import api from "../../pages/api";
+import api from "services/api";
 
-// export const GetHotelsList = (id: number) => async (dispatch: any) => {
-//   try {
-//     const res = await api.get(`/avail/${id}`);
-//     dispatch({ type: "HOTELS_LIST_SUCCESS", payload: res.data });
-//   } catch (e) {
-//     dispatch({ type: "HOTELS_LIST_FAIL" });
-//   }
-// };
+export const GetHotelsList = (id: number) => async (dispatch: any) => {
+  try {
+    const res = await api.get(`/list/${id}`);
+    dispatch({ type: "HOTELS_LIST_SUCCESS", payload: res.data });
+  } catch (err) {
+    dispatch({ type: "HOTELS_LIST_FAIL" });
+  }
+};
